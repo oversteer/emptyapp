34a8f2052262f1751a95c218609e0ecfd54f583a
Brendan Healey <healey_brendan@yahoo.co.uk>
2019-06-28 12:12:58 +0100

    Text change

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            The live version of the application will be available on Tuesday 2nd July 2019.
+            The live version of the application will be available on Tuesday 2nd July 2019.<br />
+            Apologies for any inconvenience.
           </p>
         </header>
       </div>
